90d5c7400c495d4d4410dbdfc6565ba3c012a2c5
Nathan Harris <nathan.harris.1@outlook.com>
2023-01-04 16:36:19 +0000

    basic map functionality added

--- adventure-map/src/App.js
+++ adventure-map/src/App.js
@@ -1,19 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './components/Home';
-import Register from './components/Register';
+import Home from "./components/Home";
+import Register from "./components/Register";
+import ExploreWorld from "./components/ExploreWorld";
+import ExploreContinent from "./components/ExploreContinent";
 
-import './App.css';
+import "./App.css";
 
-window.Buffer = window.Buffer || require('buffer').Buffer;
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
+        //navbar??
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="/explorecontinent/:id" element={<ExploreWorld />} /> */}
+          <Route path="/explorecontinent/:id" element={<ExploreContinent />} />
+          <Route path="/exploreworld" element={<ExploreWorld />} />
         </Routes>
       </Router>
     </div>
---
+++ adventure-map/src/components/ExploreWorld.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Tooltip } from "react-tooltip";
+import { useNavigate } from "react-router-dom";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
+
+import { continentsGeoURL } from "../mapping/continents.js";
+
+//TODO tool tip by mouse
+const colors = {
+  North_America: "#3366ff",
+  South_America: "#cb32ff",
+  Europe: "#34ff67",
+  Africa: "#ff6632",
+  Asia: "#34fecc",
+  Australia: "#ffce26",
+  Oceania: "#ffce26",
+  Antarctica: "#d3b794",
+};
+//TODO default border remove
+//TODO take out styles and put in sass??
+
+// export default function ExploreWorld() {
+export default function Home() {
+  const navigate = useNavigate();
+  const navigateToContinent = () =>
+    navigate(`/explorecontinent/${geo.properties.CONTINENT}`);
+  const [content, setContent] = useState("");
+  // function handleClick(geo) {
+  //   console.log(geo.properties);
+  //   console.log(geo.properties.CONTINENT);
+  // }
+  // const
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Map</h1>
+      <Tooltip followCursor={true}>{content}</Tooltip>
+      <div
+        style={{
+          width: "75%",
+          height: "65%",
+          borderStyle: "double",
+        }}
+      >
+        <ComposableMap data-tip="">
+          <ZoomableGroup zoom={1}>
+            <Geographies geography={continentsGeoURL}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    geography={geo}
+                    // fill="#EAEAEC"
+                    fill={colors[geo.properties.CONTINENT]}
+                    // stroke="grey"
+                    style={{
+                      default: { outline: "none" },
+                      hover: { outline: "none" },
+                      pressed: { outline: "none" },
+                    }}
+                    key={geo.rsmKey}
+                    onClick={() => navigateToContinent(geo)}
+                    onMouseEnter={() => {
+                      const { CONTINENT } = geo.properties;
+                      setContent(`${CONTINENT}`);
+                    }}
+                    onMouseLeave={() => {
+                      setContent("");
+                    }}
+                  />
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    </div>
+  );
+}
