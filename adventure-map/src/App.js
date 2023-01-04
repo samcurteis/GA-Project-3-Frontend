d0b6198e251a31d535f6261b01d47b11d80c53cb
Jet <thazledine@hotmail.co.uk>
2023-01-04 16:29:04 +0000

    start of front

--- adventure-map/src/App.js
+++ adventure-map/src/App.js
@@ -2,18 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Register from './components/Register';
+import Login from './components/Login';
 
 import './App.css';
 
 window.Buffer = window.Buffer || require('buffer').Buffer;
 
 function App() {
+  fetch('http://localhost:8080/api/countries')
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
   return (
     <div className='App'>
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </div>
