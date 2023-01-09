329dbfd1de5140080a169672eab32514d62ce713
Nathan Harris <nathan.harris.1@outlook.com>
2023-01-09 14:31:47 +0000

    home page problems

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 export default function Home() {
   return (
-    <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
+    <h1> not sure what happened</h1>
+    // <ComposableMap>
+    //   <Geographies geography={geoUrl}>
+    //     {({ geographies }) =>
+    //       geographies.map((geo) => (
+    //         <Geography key={geo.rsmKey} geography={geo} />
+    //       ))
+    //     }
+    //   </Geographies>
+    // </ComposableMap>
   );
 }
