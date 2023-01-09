c8bb1edc01572ec336f663d1aedb342ea8389411
Jet <thazledine@hotmail.co.uk>
2023-01-09 14:30:10 +0000

    homepage?

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <section className='heroPage'>
       <div>
-        <h1 className='importantTitles'> Adventure Map</h1>
-        <h3 className='importantTitles'> Explore the world</h3>
+        <h1 className='importantTitles maintitle'> Adventure Map</h1>
+        <h3 className='importantTitles subtitle'> Explore the world</h3>
+        <h3 className='importantTitles subtitle'> Share your discoveries</h3>
       </div>
     </section>
   );
