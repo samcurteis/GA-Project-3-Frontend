12efd18648010b7d8219f497ca5b5dd78a6ad7bf
samcurteis <samcurteis@gmail.com>
2023-01-26 15:20:35 +0000

    added react app base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,16 +2,19 @@ import axios from 'axios';
 import { AUTH } from './auth';
 
 const ENDPOINTS = {
-  allCountries: '/api/countries',
-  singleCountry: (id) => `/api/countries/${id}`,
-  searchCountries: (query) => `/api/countries/search?search=${query}`,
-  allEntries: `/api/entries`,
-  singleEntry: (id) => `/api/entries/${id}`,
-  allUsers: '/api/users',
-  singleUser: (id) => `/api/users/${id}`,
-  searchUsers: (query) => `/api/users/search?search=${query}`,
-  login: '/api/login',
-  register: '/api/register',
+  allCountries: `${process.env.REACT_APP_BASE_URL}/api/countries`,
+  singleCountry: (id) =>
+    `${process.env.REACT_APP_BASE_URL}/api/countries/${id}`,
+  searchCountries: (query) =>
+    `${process.env.REACT_APP_BASE_URL}/api/countries/search?search=${query}`,
+  allEntries: `${process.env.REACT_APP_BASE_URL}/api/entries`,
+  singleEntry: (id) => `${process.env.REACT_APP_BASE_URL}/api/entries/${id}`,
+  allUsers: `${process.env.REACT_APP_BASE_URL}/api/users`,
+  singleUser: (id) => `${process.env.REACT_APP_BASE_URL}/api/users/${id}`,
+  searchUsers: (query) =>
+    `${process.env.REACT_APP_BASE_URL}/api/users/search?search=${query}`,
+  login: `${process.env.REACT_APP_BASE_URL}/api/login`,
+  register: `${process.env.REACT_APP_BASE_URL}/api/register`,
   cloudinary: `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`
 };
 
